9f6f23bd9a780a7411c9eb6a0f2e75241d0d8011
WalterRB28 <00018523@uca.edu.sv>
2026-05-09 09:04:45 -0600

    Adding base prohject for lab 2

---
+++ labo_dos_n_capas/src/main/java/com/ncapas/labo_dos_n_capas/LaboDosNCapasApplication.java
@@ -0,0 +1,13 @@
+package com.ncapas.labo_dos_n_capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboDosNCapasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaboDosNCapasApplication.class, args);
+    }
+
+}
---
+++ labo_dos_n_capas/src/test/java/com/ncapas/labo_dos_n_capas/LaboDosNCapasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncapas.labo_dos_n_capas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaboDosNCapasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
